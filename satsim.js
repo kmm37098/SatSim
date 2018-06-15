21367c8fe79b1bb14c6509dda199d180d69ac503
Kyle Millar <kymilla608@gmail.com>
2018-06-15 17:59:35 -0400

    Comments and add point of reference

--- satsim.js
+++ satsim.js
@@ -1,19 +1,24 @@
-var swathX = 300;
-var swathY = 184; 
-var tlPoint = 0;
+var swathX = 300.52;	
+var swathY = 184;	
+var pRefX = 300.52; //front of swath
+var pRefY = 480;  //middle of swath
+
+var tlPoint = 0;	//area coordinates
 var blPoint = 0;
 var trPoint = 0;
 var brPoint = 0;
-var startScan = 540;
-var endScan = 741;
-var simulate = false;
+
+var startScan = 540.52;	//point at which the scan starts
+var endScan = 741.52;		//point at which the scan ends
+	
+var simulate = false;	
 var inRange = true;
-var set = true;
+var set = true;			
 
 
-var spareRandom = null;
+var spareRandom = null;		//used for generating random
 
-var pSlider;
+var pSlider;	//user input
 var pInput;
 var rSlider;
 var rInput;
@@ -24,12 +29,14 @@ function setup() {
   createCanvas(1280, 960);
   textSize(15);
 
+  //setting up sliders and checkboxes
+
   pSlider = createSlider(-3.4908, 3.4908, 0, 0); //set up sliders and text boxes
   pSlider.position(20, 20);						 	
   pInput = createInput();
   pInput.position(280,20);
   pInput.size(50,17);
-  rSlider = createSlider(-5, .5, 0, 0);
+  rSlider = createSlider(-.5, .5, 0, 0);
   rSlider.position(20, 50);
   rInput = createInput();
   rInput.position(280,50);
@@ -50,6 +57,11 @@ function setup() {
 
 function draw() {
   background(51);
+
+  //--------------------------------------------------------------------------
+  //     Grab and display values for pitch, roll, yaw, and set instability
+  //--------------------------------------------------------------------------
+
 
   let instability = false;			//instability yes or no?
   let instabilityVal = 0;
@@ -86,16 +98,22 @@ function draw() {
   text("Press 's' to start simulation.", 1000, 900);
   text("Press 'r' to reset simulation.", 1000, 930);
 
+  //------------------------------------------------------------------------------------------------------------------
+  //      Display target area. Set position of swath, reference point, startScan, and endScan according to errors
+  //------------------------------------------------------------------------------------------------------------------
+
   if(set) {
-  	swathX = 300 + pErr;		//set orientation of swath (no instability accounted)
+  	swathX = 300.52 + pErr;		//set orientation of swath (no instability accounted)
   	swathY = 184 + yErr;
-  	startScan = 540 + pErr;		//set start and end scan position 
-  	endScan = 740 + pErr;
+  	pRefX = 300.52 + pErr;		//set point of reference
+  	pRefY = 480 + yErr;
+  	startScan = 540.52 + pErr;		//set start and end scan position 
+  	endScan = 741.52 + pErr;
   }
   
   fill('green');		//display target area
   stroke('green');
-  let tArea = rect(540,360,200,200);
+  let tArea = rect(540.52,360,200,200);
 
   fill('red');			//display swath w/ errors
   stroke('red');
@@ -103,9 +121,17 @@ function draw() {
   translate(swathX,swathY);
   rotate(rErr);
   let swath = rect(0,0,.52,592);
+
+  fill('white');		//display point of reference
+  stroke('white');
+  pRef = ellipse(.52, 296, 2, 2);
   pop();
 
-  if(swathX >= startScan) {			//display initial swath scan position
+  //-------------------------------------------------
+  //     Display initial and final scan positions
+  //-------------------------------------------------
+
+  if(pRefX >= startScan) {			//display initial swath scan position
   	fill('blue');
   	stroke('blue');
   	push();
@@ -115,47 +141,65 @@ function draw() {
   	stroke('red');
   	pop();
   }
-  if(swathX >= endScan) {			//display final swath scan position
+  if(pRefX >= endScan) {			//display final swath scan position
   	fill('blue');
   	stroke('blue');
   	push();
-  	translate(endScan,swathY);
+  	translate(endScan-1.52,swathY);		//-1.52 to accomadate for endScan having to be divisible by 3
   	rotate(rErr);
   	let scan2 = rect(0,0,.52,592);
   	stroke('red');
   	pop();
   }
-  if(swathX == startScan) {			//find top left and bottom left corner of area polygon
-  	if(startScan > 540) {
-  		tlPoint += swathX;
-  		blPoint += swathX;
+
+  //---------------------------------------
+  //	CALCULATE AREA
+  //---------------------------------------
+
+
+  if(pRefX == startScan) {			//find top left and bottom left corner of area polygon
+  	if(startScan > 540.52) {
+  		tlPoint += pRefX;
+  		blPoint += pRefX;
   	}
-  	else {
-  		tlPoint += 540;
-  		blPoint += 540;
+  	else {							//UPDATE when finished
+  		tlPoint += 540.52;
+  		blPoint += 540.52;
   	}
   	if(rErr != 0) {
   		let diff = (((296 * Math.cos(getDegrees(rErr))) - yErr) - 100) * Math.tan(getDegrees(rErr));
-  		if(diff + tlPoint > 540) {
+  		if(diff + tlPoint > 540.52) {
   			tlPoint += diff;
   		}
   	}
+  	console.log(tlPoint);
   }
-  if(swathX >= endScan && swathX <= endScan + 3) {
-  	trPoint += swathX;
+  if(pRefX == endScan) {				//what is up with this???????????????????????????
+  	trPoint += pRefX;
+  	console.log(trPoint);
   }
-  //text(tlPoint, 500, 600);	//testing
-  //rect(trPoint,500,50,50);	//testing
+
+  //---------------------------------------
+  //	Conditions for swath movement
+  //---------------------------------------
 
   if(swathX > 980) {			//check to end movement of swath
   	inRange = false;
   }
   if(simulate && inRange) {		//make swath move if simulating and in range
   	swathX = swathX + 3;
+  	pRefX = pRefX + 3;
   }
 
   keyTyped();					//add listener for key typed
 }
+
+
+//-----------------------------------------
+//-----------------------------------------
+//			Helper functions
+//-----------------------------------------	
+//-----------------------------------------
 
 function keyTyped() {			//if start, no longer in setup, simulating, and in range
 	if (key === 's') {
@@ -167,6 +211,8 @@ function keyTyped() {			//if start, no longer in setup, simulating, and in range
 		simulate = false;
 		swathX = 300;
 		swathY = 184;
+		pRefX = 300.52;
+		pRefY = 480;
 		tlPoint = 0;
 		blPoint = 0;
 		trPoint = 0;
@@ -221,6 +267,6 @@ function normalRandomScaled(mean, stddev)		//gaussian random function with stdde
 	return r;
 }
 
-function getDegrees(rad) {
+function getDegrees(rad) {		//get degrees from radians for trig functions
 	return rad * Math.PI/180;
 }
